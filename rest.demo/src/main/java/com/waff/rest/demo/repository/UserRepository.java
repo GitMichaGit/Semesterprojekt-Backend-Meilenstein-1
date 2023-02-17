52f5440400f243e18512462d3c20a7b197c4f7d6
anasm20 <agrimohamad@gmail.com>
2023-02-17 12:20:50 +0100

    Comments

--- rest.demo/src/main/java/com/waff/rest/demo/repository/UserRepository.java
+++ rest.demo/src/main/java/com/waff/rest/demo/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.waff.rest.demo.model.UserModel;
 
+// Ist eine Schnittstelle zwischen meine Webseite und Datenbank
+
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     List<UserModel> findByUserType(String user_type);
-    }
+}
